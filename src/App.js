48c78cde31bf9a7980097e1b65ad7daeccd48820
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-02-17 13:52:22 +0800

    Add delete and toggle reminder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
+const defaultTasks = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "Feb 6th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "Feb 5th at 2:30pm",
+    reminder: false,
+  },
+];
 
 function App() {
-  const name = 'Brad'
+  const [tasks, setTasks] = useState(defaultTasks);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header/>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show :-)"
+      )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log("fdsfdasfsf");
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
+
+// CSS in JS
+// const headingStyle = {
+//   color: "red",
+// };
 
 Header.defaultProps = {
   title: "Task Tracker",
